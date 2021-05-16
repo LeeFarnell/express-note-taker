9cc2fce76f5dbd9c5be248df620c4b9eb1b63333
Lee Farnell <leefarnell@mac.com>
2021-05-16 16:06:32 +0100

    refactored file structure and added filepath

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 
 const PORT = 4000;
@@ -5,6 +7,10 @@ const PORT = 4000;
 const app = express();
 
 const getNotes = (req, res) => {
+  const filepath = path.join(__dirname, "/db/db.json");
+  console.log(filepath);
+  const notes = fs.readFileSync(filepath, "utf-8");
+  console.log(notes);
   res.send("Hello World!");
 };
 

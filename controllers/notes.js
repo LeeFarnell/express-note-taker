d71733d61dc85522e36c62019173c50ce4a08dc8
Lee Farnell <leefarnell@mac.com>
2021-05-19 21:31:38 +0100

    Read & write to file functions declared & working.

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,4 +1,7 @@
+const { v4: uuidv4 } = require("uuid");
+
 const getFromDb = require("../utils/getFromDb");
+const writeToDb = require("../utils/writeToDb");
 
 const getNotes = (req, res) => {
   const notes = getFromDb();
@@ -6,4 +9,15 @@ const getNotes = (req, res) => {
   res.json(notes);
 };
 
-module.exports = { getNotes };
+const writeNotes = (req, res) => {
+  const newNote = { ...req.body, id: uuidv4() };
+
+  const notes = getFromDb();
+  const data = [...notes, newNote];
+
+  writeToDb(data);
+
+  res.json(data);
+};
+
+module.exports = { getNotes, writeNotes };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
-const { getNotes } = require("../controllers/notes");
+const { getNotes, writeNotes } = require("../controllers/notes");
 const router = Router();
 
 router.get("/notes", getNotes);
+
+router.post("/notes", writeNotes);
 
 module.exports = router;
